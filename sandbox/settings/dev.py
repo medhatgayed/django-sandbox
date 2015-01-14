790724598a47e14d55f1af0b6672476a232da23a
Medhat Gayed <medhat.gayed@yellow.co.nz>
2015-01-14 16:53:33 +1300

    Settings refactor.

---
+++ sandbox/settings/dev.py
@@ -0,0 +1,12 @@
+from .common import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'sandbox',                      # Or path to database file if using sqlite3.
+        'USER': 'medhat',                      # Not used with sqlite3.
+        'PASSWORD': 'pass',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
--- sandbox/urls.py
+++ sandbox/urls.py
@@ -1,4 +1,10 @@
 from django.conf.urls import patterns, include, url
+from rest_framework import routers
+from restapp import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,4 +15,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
